535bc8062baaa2c2fec1400cf3bff22f28e9d04e
OriKosary <53228654+OriKosary@users.noreply.github.com>
2021-01-14 17:03:08 +0200

    client select

--- Client.py
+++ Client.py
@@ -1,11 +1,16 @@
-from ProjectMITM.Graphics import *
+from MITM.Graphics import *
+from MITM.encrypter import *
 import threading
 from socket import socket
 import time
+import select
+import msvcrt
+import sys
 
 
 class Client(GUI):
     def __init__(self, root=None):
+        self.e = Encrypter()
         super().__init__(root=root)
         t = threading.Thread(target=self.main)
         t.start()
@@ -26,22 +31,34 @@ class Client(GUI):
 
     def main(self, host='127.0.0.1', port=8200):
 
+        keys = []  # list of tuples key + name of sender
+        input2 = ""
+
         while self.wait:
             time.sleep(0.01)
         host = self.input_text
-        sock = socket()
 
+        client_socket = socket()
+
+        # if name not in keys then send the key aswell
         try:
-            sock.connect((host, port))
+            client_socket.connect((host, port))
             while 'connected':
-                data = input("Enter msg: ").encode()
-                sock.send(data)
+                rlist, wlist, xlist = select.select([client_socket], [client_socket], [])
+                for current_socket in rlist:
+                    data = current_socket.recv(1024)
+                    data = data.decode()
+                    if data != "":
+                        print(data)
+                    input2 = input("msg : ")
+                    current_socket.send(input2.encode())
+
         finally:
-            sock.close()
+            client_socket.close()
 
 
 if __name__ == '__main__':
     root = Tk()
     c = Client(root=root)
-    # c.main()
     root.mainloop()
+
