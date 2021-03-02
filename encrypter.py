79fdf8edad48f354cc8b6efe04db2db9d592b0df
OriKosary <53228654+OriKosary@users.noreply.github.com>
2021-03-02 22:44:02 +0200

    for testing import export keys

--- encrypter.py
+++ encrypter.py
@@ -16,10 +16,13 @@ class Encrypter:
         self.rsa_private_key = PKCS1_OAEP.new(rsa_private_key)
 
     def encrypt(self, string):
-        return self.rsa_public_key.encrypt(string)
+        string_b = str.encode(string)
+        encrypted_text = self.rsa_public_key.encrypt(string_b)
+        return encrypted_text
 
-    def decrypt(self, string):
-        return self.rsa_private_key.decrypt(string)
+    def decrypt(self, string):  # type -> Bytes
+        decrypted_text = self.rsa_private_key.decrypt(string)
+        return decrypted_text
 
 
 # def rsa_encrypt_decrypt():
@@ -32,7 +35,6 @@ class Encrypter:
 #     rsa_public_key = RSA.importKey(public_key)
 #     rsa_public_key = PKCS1_OAEP.new(rsa_public_key)
 #     encrypted_text = rsa_public_key.encrypt(message)
-#     # encrypted_text = b64encode(encrypted_text)
 #
 #     print('your encrypted_text is : {}'.format(encrypted_text))
 #
@@ -43,4 +45,5 @@ class Encrypter:
 #     print('your decrypted_text is : {}'.format(decrypted_text))
 #
 #
-# rsa_encrypt_decrypt()
+e = Encrypter()
+
